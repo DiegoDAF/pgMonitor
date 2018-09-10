1287e33841b41ed4ee2e7581ad9c7b3daee708de
diegodaf <diego@daf.com.ar>
2018-09-10 15:20:52 -0300

    Borro las carpetas viejas

--- database/01 tablespace  y database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLESPACE ts_monitoreo
-  OWNER postgres
-  LOCATION '/home/bases_postgres/tablespaces/ts_monitoreo';
-
-CREATE DATABASE db_monitoreo
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = ts_monitoreo
-       CONNECTION LIMIT = -1;
--- database/09 view vservidores.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW public.vservidores AS 
- SELECT servidores.id,
-    servidores.hostname,
-    servidores.port
-   FROM servidores
-  WHERE servidores.estado = 'AA'::bpchar;
